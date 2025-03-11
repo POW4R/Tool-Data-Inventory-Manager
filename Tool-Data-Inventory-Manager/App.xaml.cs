5f0410d1b94879f037eca31f5cc0605bf50cab35
Krisz <torokkrisztofer46@gmail.com>
2025-03-11 13:42:27 +0100

    feat: add basic wpf project.

---
+++ Tool-Data-Inventory-Manager/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Tool_Data_Inventory_Manager
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
