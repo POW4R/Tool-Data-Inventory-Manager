f589181c9b2c72c9324923730efbde4e494657e8
POW4R <slemmerkevin@gmail.com>
2025-05-02 21:50:19 +0200

    feat: add simple logging

--- Tool-Data-Inventory-Manager/App.xaml.cs
+++ Tool-Data-Inventory-Manager/App.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System.Configuration;
+﻿using System.Collections.ObjectModel;
 using System.Data;
 using System.Windows;
+using Serilog;
+using Serilog.Sinks.MSSqlServer;
 
 namespace Tool_Data_Inventory_Manager
 {
@@ -9,8 +11,42 @@ namespace Tool_Data_Inventory_Manager
     /// </summary>
     public partial class App : Application
     {
-        public App() {
-        
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var sinkOpts = new MSSqlServerSinkOptions
+            {
+                TableName = "Logs",
+                AutoCreateSqlTable = true
+            };
+
+            var columnOpts = new ColumnOptions
+            {
+                AdditionalColumns = new Collection<SqlColumn>
+            {
+                new SqlColumn("Username", SqlDbType.NVarChar, dataLength: 100),
+                new SqlColumn("EventContext", SqlDbType.NVarChar, dataLength: 250)
+            }
+            };
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.MSSqlServer(
+                    connectionString: "Server=localhost\\SQLEXPRESS;Database=Inventory Manager;Trusted_Connection=True;Encrypt=False;",
+                    sinkOptions: sinkOpts,
+                    columnOptions: columnOpts
+                )
+                .Enrich.WithProperty("AppName", "InventoryManager")
+                .CreateLogger();
+
+            Log.Information("Alkalmazás elindult.");
+            base.OnStartup(e);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            Log.Information("Alkalmazás leáll.");
+            Log.CloseAndFlush();
+            base.OnExit(e);
         }
     }
 
